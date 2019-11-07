9eb04baa2215f9577992b6b1d4ee11b603cbc760
Tobias Macbook <tobiaslkj@gmail.com>
2019-11-07 13:36:21 +0800

    changed db connection

--- Connection.py
+++ Connection.py
@@ -6,11 +6,45 @@ class Connection:
     def __init__(self):
         with open('conf.json', 'r') as f:
             conf = json.load(f)
-            self.conn = psycopg2.connect(dbname=conf['db']['dbname'], user=conf['db']['username'],
-                                         password=conf['db']['password'], host=conf['db']['host'],
-                                         port=conf['db']['port'])
-            self.cursor = self.conn.cursor()
+            self.dbname = conf['db']['dbname']
+            self.username = conf['db']['username']
+            self.password = conf['db']['password']
+            self.host = conf['db']['host']
+            self.port = conf['db']['port']
+            self.connection = None
+            self.cursor = None
+    """
+    Use this method if you wanna overwrite any db configs
+    pass in a dict with items you wanna override
+    - dbname
+    - username
+    - password
+    - host
+    - port (integer)
+    """
+    def override_configuration(self, **kwargs):
+        if 'dbname' in kwargs:
+            self.dbname = kwargs['dbname']
+        if 'username' in kwargs:
+            self.username = kwargs['username']
+        if 'password' in kwargs:
+            self.password = kwargs['password']
+        if 'host' in kwargs:
+            self.host = kwargs['host']
+        if 'port' in kwargs:
+            self.port = kwargs['port']
 
+    """
+    Call this method when you ready to connect to db
+    """
+    def connect(self):
+        self.conn = psycopg2.connect(dbname=self.dbname, user=self.username, password=self.password, host=self.host,
+                                     port=self.port)
+        self.cursor = self.conn.cursor()
+
+    """
+    return an instance of the connection
+    """
     def get_connection(self):
         return self.conn
 
@@ -19,4 +53,4 @@ class Connection:
 
     def query_json(self, statement):
         self.cursor.execute("EXPLAIN (FORMAT JSON) " + statement)
-        return self.cursor.fetchall()[0][0][0]
+        return self.cursor.fetchall()[0][0]
--- main.py
+++ main.py
@@ -1,11 +1,57 @@
 from Connection import Connection
+import json
+from parser import parse_json
+from pprint import pprint
+import svgling
+from parser import Node
 
 
-conn = Connection()
-data = conn.query_json("SELECT * FROM customer;")
-print(data)
+def main():
+    conn = Connection()
+    conn.override_configuration(host="localhost", password="dsp")
+    conn.connect()
+    query = """
+    select
+        orders_id,
+        sum(extended_price * (1 - discount)) as revenue,
+        order_date,
+        ship_priority
+    from
+        customer,
+        orders,
+        lineitem
+    where
+        market_segment = 'HOUSEHOLD'
+        and customer.id = orders.customer_id
+        and lineitem.orders_id = orders.id
+        and orders.order_date < date '1995-03-21'
+        and lineitem.ship_date > date '1995-03-21'
+    group by
+        lineitem.orders_id,
+        orders.order_date,
+        orders.ship_priority
+    order by
+        revenue desc,
+        orders.order_date
+    limit 10;
+    """
 
-data2 = conn.query_json("SELECT * FROM region")
-print(data2)
+    data = conn.query_json(query)
+    node = parse_json(json.dumps(data))
+
+    pprint(vars(node.children[0]))
+
+    print(print_node(node))
 
 
+def print_node(node):
+    ls = [node.node_type]
+
+    for n in node.children:
+        ls.append(print_node(n))
+
+    return ls
+
+
+if __name__ == '__main__':
+    main()
