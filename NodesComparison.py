86242388388b77fccb7eab148c48a63109199335
Harrison Wong Jun Yong <Harrisonwjy@hotmail.com>
2019-11-15 15:26:08 +0800

    Remove comment and add some documentation for NodesComparison

--- NodesComparison.py
+++ NodesComparison.py
@@ -10,6 +10,9 @@ class NodesComparison:
         self.list_joins = ['Nested Loop', 'Hash Join', 'Merge Join']
         self.unique_tables_for_both_query = []
 
+    """
+    recursive function to get all the node based on the node types
+    """
     def get_all_node_type(self, node, request_node_type):
         if node.node_type in request_node_type:
             self.store_get_all_nodes_type.append(node)
@@ -20,7 +23,6 @@ class NodesComparison:
     recursive function to get the tables used for join
     access self.store_get_tables_for_join for the list of tables used for join
     """
-
     def get_tables_for_join(self, node):
         if node.relation_name != None:
             self.store_get_tables_for_join.append(node)
@@ -30,13 +32,13 @@ class NodesComparison:
                 self.get_tables_for_join(child_node)
 
     ## get tables node for a join
-    def get_tables_for_join2(self, node):
+    def get_tables_for_join(self, node):
         if node.relation_name != None:
             self.store_get_tables_for_join.append(node)
 
         if node.children != None:
             for child_node in node.children:
-                self.get_tables_for_join2(child_node)
+                self.get_tables_for_join(child_node)
 
     # populate the table attribute for those node type has a join.
     # populate with the nodes.
@@ -44,7 +46,7 @@ class NodesComparison:
         # if node.node_type is in one of the joins
         if node.node_type in self.list_joins:
             self.store_get_tables_for_join.clear()
-            self.get_tables_for_join2(node)
+            self.get_tables_for_join(node)
             node.tables = self.store_get_tables_for_join.copy()
 
         for child in node.children:
@@ -95,69 +97,16 @@ class NodesComparison:
         self.get_all_node_type(self.head_node_2, self.list_joins)
         all_join_nodes_in_query_2 = self.store_get_all_nodes_type.copy()
 
-        # print("all join nodes in query 1:")
-        # print(all_join_nodes_in_query_1)
-        # for node in all_join_nodes_in_query_1:
-        #     print(node.node_type)
-        #     for table in node.tables:
-        #         print(table.relation_name)
-        #
-        # print("all join nodes in query 2:")
-        # print(all_join_nodes_in_query_2)
-        # for node in all_join_nodes_in_query_2:
-        #     print(node.node_type)
-        #     for table in node.tables:
-        #         print(table.relation_name)
-
-        # print("List of joins in query 1:")
-        # for node in self.store_get_all_nodes_type_1:
-        #     print(node.node_type)
-        #     for child in node.children:
-        #         print(child.node_type)
-        #     print("----")
-        #
-        # print("List of joins in query 2:")
-        # for node in self.store_get_all_nodes_type_2:
-        #     print(node.node_type)
-        #     for child in node.children:
-        #         print(child.node_type)
-        #     print("----")
-
-        # check for different join, and join with different tables
-
-        # print("List of joins and their tables for first query:")
-        # print(nodes_and_tables_1)
-        # for nodes in nodes_and_tables_1:
-        #     print(nodes[0].node_type)
-        #     for child_nodes in nodes[1]:
-        #         print(child_nodes.node_type + " on "+child_nodes.relation_name)
-        #     print("---")
-        # print("List of joins and their tables for second query:")
-        # print(nodes_and_tables_2)
-        # for nodes in nodes_and_tables_2:
-        #     print(nodes[0].node_type)
-        #     for child_nodes in nodes[1]:
-        #         print(child_nodes.node_type + " on "+child_nodes.relation_name)
-        #     print("---")
-
         # We reverse the list so, the biggest node (in terms of no. of table joins - usually is the must top level of join node)
         # is at the back of the list.
         all_join_nodes_in_query_1.reverse()
         all_join_nodes_in_query_2.reverse()
-        # print("--------------")
-        # for node1 in all_join_nodes_in_query_1:
-        #     print(node1.node_type)
-        #
-        # for node2 in all_join_nodes_in_query_2:
-        #     print(node2.node_type)
-        # print("--------------")
 
         all_join_node_full_difference = []
         all_join_nodes_in_query_2_duplicate = all_join_nodes_in_query_2.copy()
         found = False
         for node2 in all_join_nodes_in_query_2:
             for node1 in all_join_nodes_in_query_1:
-                #print(node1.node_type + "("+str(node1.level)+") compare with " + node2.node_type + "("+str(node2.level)+")")
                 if self.is_there_similiarities(node1, node2):
                     # check what is the difference between this two join nodes,
                     # and check whether they have the same table used, and the types of scan they used.
@@ -166,9 +115,6 @@ class NodesComparison:
                     # store all the tables in the array so that for the next loop, it WILL NOT print duplicate changes.
                     self.store_tables(node1)
                     self.store_tables(node2)
-
-                    # print("Unique tables:")
-                    # print(self.unique_tables_for_both_query)
 
                     # We remove from the nodes involved over here from the lists.
                     all_join_nodes_in_query_1.remove(node1)
@@ -256,25 +202,6 @@ class NodesComparison:
             if found == False:
                 table_pairs_nodes = self.insert_table_pairs(node1, None, table_pairs_nodes)
 
-        # print(dependency_nodes_2)
-
-        # Check the type of joins first.
-        # If the join is difference, we display it out.
-
-
-        # use self.unique_tables_for_both_query and check if it exist. If so, we dont display the changes.
-
-
-
-        # print(table_pairs)
-        # for pair in table_pairs:
-        #     for inner_pair in pair:
-        #         for inner_2_pair in inner_pair:
-        #             print(inner_2_pair.relation_name + " " + inner_2_pair.node_type)
-
-        # print("dependency_nodes")
-        # print(dependency_nodes_2[0].relation_name)
-
         # remove all matching scan type of the tables for both query
         table_pairs = table_pairs_nodes.copy()
         for pair in table_pairs:
@@ -287,7 +214,6 @@ class NodesComparison:
                             if (node1.node_type == node2.node_type):
                                 pair[0].remove(node1)
                                 pair[1].remove(node2)
-                                # if is empty array for both pair[0] and pair[1], we can remove this pair from table_pairs
 
         # remove empty pairs
         i = 0
@@ -306,10 +232,6 @@ class NodesComparison:
                         diffString += "With the evolved "+current_node.node_type+ ", the type of scan has evolve from " + node1.node_type + " to " + node2.node_type + " for the table, " + node1.relation_name + "\n"
                         pair[0].remove(node1)
                         pair[1].remove(node2)
-
-                        # if is empty array for both pair[0] and pair[1], we can remove this pair from table_pairs
-                        # if not(len(pair[0]) > 0 and len(pair[1]) > 0):
-                        #     table_pairs.remove(pair)
 
         # remove those empty pairs E.g [[],[]]
         i = 0
@@ -340,35 +262,20 @@ class NodesComparison:
         string_table_used_in_q1_but_not_in_q2 = string_table_used_in_q1_but_not_in_q2.strip()
 
 
+        # if there are tables in used in q1 and tables used in q2. We know that there tables in q1 is not used in q2.
         if tables_used_in_q1_only and tables_used_in_q2_only:
             diffString += "It seems like the tables ("+string_table_used_in_q1_but_not_in_q2 + \
                   ") for "+previous_node.node_type+" (Level "+str(previous_node.level) + \
                   ") in Query 1 is replaced with tables ("+string_table_used_in_q2_but_not_in_q1 + ") for "+current_node.node_type+" (Level "+str(current_node.level)+") in Query 2" + "\n"
         elif(not tables_used_in_q1_only and tables_used_in_q2_only):
+            # If there is no tables in q1, and there is table in q2. We can say that there are new tables used in Q2
             diffString += "It seems like that there is new tables ("+string_table_used_in_q2_but_not_in_q1+") used in Q2" + "\n"
         elif(tables_used_in_q1_only and not tables_used_in_q2_only):
+            # If there is tables in q1 but there is no table in q2. We can say that those tables in Q1 is not in Q2
             diffString += "It seems like the "+current_node.node_type+" (Level "+str(current_node.level)+") for Query 2 did not use the tables ("+string_table_used_in_q1_but_not_in_q2+") used by the "+previous_node.node_type+" (Level "+str(previous_node.level)+") in Query 1 at all."+"\n"
 
         return diffString
 
-    # def insert_table_pairs(self, node1, node2, table_pairs):
-    #     found = False
-    #     for pair in table_pairs:
-    #         # if found, append into the pair
-    #         if node1.relation_name in pair[0]:
-    #             found = True
-    #             pair[0].append(node1.relation_name)
-    #             if node2 != None:
-    #                 pair[1].append(node2.relation_name)
-    #
-    #     # if not found, create a new pair.
-    #     if found == False:
-    #         if node2 != None:
-    #             table_pairs.append([[node1.relation_name], [node2.relation_name]])
-    #         else:
-    #             table_pairs.append([[node1.relation_name], []])
-    #
-    #     return table_pairs
 
     def insert_table_pairs(self, node1, node2, table_pairs):
         found = False
